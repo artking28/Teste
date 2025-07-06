6095ab5eaa45fad61c8b1af6388718df95b4b561
a.andrade <arthur10andrade@gmail.com.br>
2025-07-06 13:25:29 -0300

    .

--- server/src/main/java/org/tokio/teste/arthur/config/DataSeeder.java
+++ server/src/main/java/org/tokio/teste/arthur/config/DataSeeder.java
@@ -3,6 +3,7 @@ package org.tokio.teste.arthur.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.tokio.teste.arthur.domain.entity.City;
 import org.tokio.teste.arthur.domain.entity.State;
 import org.tokio.teste.arthur.repository.ICityRepository;
@@ -16,7 +17,48 @@ import java.util.stream.Collectors;
 
 @Configuration
 public class DataSeeder {
+
     @Bean
+    @Order(0)
+    CommandLineRunner seed(IStateRepository repo) {
+        return args -> {
+            if (repo.count() != 27) {
+                repo.deleteAll();
+                repo.saveAll(List.of(
+                        new State("AC", "Acre"),
+                        new State("AL", "Alagoas"),
+                        new State("AP", "Amapá"),
+                        new State("AM", "Amazonas"),
+                        new State("BA", "Bahia"),
+                        new State("CE", "Ceará"),
+                        new State("DF", "Distrito Federal"),
+                        new State("ES", "Espírito Santo"),
+                        new State("GO", "Goiás"),
+                        new State("MA", "Maranhão"),
+                        new State("MT", "Mato Grosso"),
+                        new State("MS", "Mato Grosso do Sul"),
+                        new State("MG", "Minas Gerais"),
+                        new State("PA", "Pará"),
+                        new State("PB", "Paraíba"),
+                        new State("PR", "Paraná"),
+                        new State("PE", "Pernambuco"),
+                        new State("PI", "Piauí"),
+                        new State("RJ", "Rio de Janeiro"),
+                        new State("RN", "Rio Grande do Norte"),
+                        new State("RS", "Rio Grande do Sul"),
+                        new State("RO", "Rondônia"),
+                        new State("RR", "Roraima"),
+                        new State("SC", "Santa Catarina"),
+                        new State("SP", "São Paulo"),
+                        new State("SE", "Sergipe"),
+                        new State("TO", "Tocantins")
+                ));
+            }
+        };
+    }
+
+    @Bean
+    @Order(1)
     CommandLineRunner seedCities(ICityRepository cityRepo, IStateRepository stateRepo) {
         return args -> {
             if (cityRepo.count() == 0) {
@@ -51,44 +93,6 @@ public class DataSeeder {
 
                     cityRepo.saveAll(cities);
                 }
-            }
-        };
-    }
-
-    @Bean
-    CommandLineRunner seed(IStateRepository repo) {
-        return args -> {
-            if (repo.count() != 27) {
-                repo.deleteAll();
-                repo.saveAll(List.of(
-                        new State("AC", "Acre"),
-                        new State("AL", "Alagoas"),
-                        new State("AP", "Amapá"),
-                        new State("AM", "Amazonas"),
-                        new State("BA", "Bahia"),
-                        new State("CE", "Ceará"),
-                        new State("DF", "Distrito Federal"),
-                        new State("ES", "Espírito Santo"),
-                        new State("GO", "Goiás"),
-                        new State("MA", "Maranhão"),
-                        new State("MT", "Mato Grosso"),
-                        new State("MS", "Mato Grosso do Sul"),
-                        new State("MG", "Minas Gerais"),
-                        new State("PA", "Pará"),
-                        new State("PB", "Paraíba"),
-                        new State("PR", "Paraná"),
-                        new State("PE", "Pernambuco"),
-                        new State("PI", "Piauí"),
-                        new State("RJ", "Rio de Janeiro"),
-                        new State("RN", "Rio Grande do Norte"),
-                        new State("RS", "Rio Grande do Sul"),
-                        new State("RO", "Rondônia"),
-                        new State("RR", "Roraima"),
-                        new State("SC", "Santa Catarina"),
-                        new State("SP", "São Paulo"),
-                        new State("SE", "Sergipe"),
-                        new State("TO", "Tocantins")
-                ));
             }
         };
     }
